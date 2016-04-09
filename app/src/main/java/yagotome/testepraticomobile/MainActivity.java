6df7c9105f5457b86dd33c94393ba3f019e08ee1
Yago Gomes Tomé de Sousa <yago.tome@hotmail.com>
2016-04-08 22:59:30 -0300

    Preparar action bar no layout da activity principal.

--- app/src/main/java/yagotome/testepraticomobile/MainActivity.java
+++ app/src/main/java/yagotome/testepraticomobile/MainActivity.java
@@ -19,13 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Clicou no FAB.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
     }
 
     @Override
