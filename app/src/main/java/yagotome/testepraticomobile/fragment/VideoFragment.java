bbba077abb8bbb01f016cab7821b86391535c576
Yago Gomes Tomé de Sousa <yago.tome@hotmail.com>
2016-04-12 22:51:52 -0300

    Criar visualização de projetos que são videos.

--- app/src/main/java/yagotome/testepraticomobile/fragment/VideoFragment.java
+++ app/src/main/java/yagotome/testepraticomobile/fragment/VideoFragment.java
@@ -1,9 +1,13 @@
 package yagotome.testepraticomobile.fragment;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.devbrackets.android.exomedia.EMVideoView;
 
 import yagotome.testepraticomobile.R;
 import yagotome.testepraticomobile.domain.Video;
@@ -11,8 +15,10 @@ import yagotome.testepraticomobile.domain.Video;
 /**
  *
  */
-public class VideoFragment extends BaseFragment {
+public class VideoFragment extends BaseFragment implements MediaPlayer.OnPreparedListener {
+    private static final String TAG = "VideoFragment";
     private Video video;
+    private  EMVideoView emVideoView;
 
     public VideoFragment() {
     }
@@ -28,4 +34,27 @@ public class VideoFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_video, container, false);
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupVideoView();
+    }
+
+    private void setupVideoView() {
+        assert (getView() != null);
+        emVideoView = (EMVideoView)getView().findViewById(R.id.video_play_activity_video_view);
+        emVideoView.setOnPreparedListener(this);
+
+        //For now we just picked an arbitrary item to play.  More can be found at
+        //https://archive.org/details/more_animation
+        emVideoView.setVideoURI(Uri.parse("https://archive.org/download/Popeye_forPresident/Popeye_forPresident_512kb.mp4"));
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        //Starts the video playback as soon as it is ready
+        emVideoView.start();
+    }
+
 }
